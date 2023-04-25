841a87201f1faa02834567f0f9baf1f4c027162d
Tolu Eshiet <tmeshiet@gmail.com>
2023-04-25 14:43:01 -0600

    Updated temperatue

--- conditional.js
+++ conditional.js
@@ -11,7 +11,7 @@ if (cold) {
 console.log("Now you are ready to go outside!");
 
 
-const temperature = 20;
+const temperature = 50;
 if(temperature <0) {
 console.log("Make sure you pick a scarf!");
 } else if (temperature < 15) {
@@ -26,6 +26,10 @@ const age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn;t such a great idea...");
 }
 if (!raining) {
   console.log("Leave your umbrella at home!");
